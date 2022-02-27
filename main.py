971ab17687844ab6b1dcc33a0481740daba61a76
3XTR4OS <84981999+3XTR4OS@users.noreply.github.com>
2022-02-27 20:43:34 +1000

    Update main.py

--- main.py
+++ main.py
@@ -28,24 +28,25 @@ def params_handler(row):
     else:
         state = states[row['state']['status']]
 
-    # allowed_actions - Доступные действия
+    # allowed_actions - доступные действия
     if len(row['allowedActions']) == 0:
         allowed_actions = 'Пуст'
     else:
         allowed_actions = [i['name'] for i in row['allowedActions']]
 
-    # allowedBlocks - Доступные к отображению блоки
+    # allowedBlocks - доступные к отображению блоки
     if len(row['allowedBlocks']) == 0:
         allowed_blocks = 'Пуст'
     else:
         allowed_blocks = [blocks[i] for i in row['allowedBlocks']]
 
-    # СОЗДАТЬ ДИАГРАММУ ПО ПОЛУЧЕННЫМ ПАРАМЕТРАМ
-    diag_creator.diagram_creator(user_role, state, allowed_actions, allowed_blocks)
+    return user_role, state, allowed_actions, allowed_blocks
 
 
 with open(file_path, 'r+', encoding='utf-8') as file:
     file = json.load(file)
 
+    # diag_creator.create_diagram(params_handler(file[-1]))
     for row in file:
-        params_handler(row)
+        # print(params_handler(row))
+        diag_creator.create_diagram(params_handler(row), p_outformat='png')
